9db602799e8ff9a7dfdc97dc6a9374de62c70ad1
Sumit Dubey <sumitstudyguide@gmail.com>
2026-03-25 23:37:29 +0530

    some fixes

--- frontend/utils/HotelInfo.ts
+++ frontend/utils/HotelInfo.ts
@@ -3,12 +3,12 @@
 const hotelInfo = {
     name: "Hotel Shyama Palace",
     address: "Atal Chowk, Near, in front of AdiShakti Car Wash, Vindhyachal, Mirzapur, Uttar Pradesh 231307",
-    phone: "+91 98390 47080",
-    email: "[EMAIL_ADDRESS]",
-    website: "https://hotelshyama.com",
+    phone: "+91 8009212634",
+    email: "official.hotelshyamapalace@gmail.com",
+    website: "https://hotelshyamapalace.in",
     logo: "/logo.png",
     map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3575.535322094772!2d82.56931387523778!3d25.14510017760556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398feb295eb96f3d%3A0xee9830b1ace1c056!2sHotel%20Shyama%20International%2C%20Atal%20Chowk%2C%20Near%2C%20in%20front%20of%20AdiShakti%20Car%20Wash%2C%20Vindhyachal%2C%20Mirzapur%2C%20Uttar%20Pradesh%20231307!5e0!3m2!1sen!2sin!4v1758732940371!5m2!1sen!2sin",
-    whatsapp: "+91 98390 47080",
+    whatsapp: "+91 8009212634",
 
 }
 
